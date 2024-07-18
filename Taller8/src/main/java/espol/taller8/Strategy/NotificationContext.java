62f46ed40e217c3e1038f98a2921612b2041d900
Kevin Mejia <kesameji@espol.edu.ec>
2024-07-17 19:51:03 -0500

    cambios finales Strategy

--- Taller8/src/main/java/espol/taller8/Strategy/NotificationContext.java
+++ Taller8/src/main/java/espol/taller8/Strategy/NotificationContext.java
@@ -1,16 +1,21 @@
 package espol.taller8.Strategy;
 
+import espol.taller8.Cliente;
+
 public class NotificationContext {
-    
+
     private NotificationStrategy notification;
     private Cliente cliente;
-    
-    public void setNotificationStrategy(NotificationStrategy _notification, Cliente _cliente){
+
+    public void setNotificationStrategy(NotificationStrategy _notification) {
         notification = _notification;
+    }
+
+    public void setCliente(Cliente _cliente) {
         cliente = _cliente;
     }
-    
-    public void sendNotification(){
-        notification.Notify();
+
+    public void sendNotification() {
+        notification.Notify(cliente);
     }
 }
--- Taller8/src/main/java/espol/taller8/Strategy/NotificationStrategy.java
+++ Taller8/src/main/java/espol/taller8/Strategy/NotificationStrategy.java
@@ -1,5 +1,7 @@
 package espol.taller8.Strategy;
 
+import espol.taller8.Cliente;
+
 public interface NotificationStrategy {
     
     public void Notify(Cliente cliente);
