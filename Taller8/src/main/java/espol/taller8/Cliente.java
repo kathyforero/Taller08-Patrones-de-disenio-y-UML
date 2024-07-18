6b9afb178e7976bc26bb9725262b5c657dd6cbba
Cecilia Ortiz <ceelorti@espol.edu.ec>
2024-07-17 19:46:18 -0500

    Observer

---
+++ Taller8/src/main/java/espol/taller8/Cliente.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package espol.taller8;
+
+/**
+ *
+ * @author Cecy
+ */
+public abstract class Cliente {
+    protected String nombre;
+    protected String cedula;
+           
+    public void update(){
+    //Todo
+    }
+}
